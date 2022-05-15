924d7ca0c1daf8dcf6234f65bb7295f2ba8cc511
tsuccar <tsuccar@users.noreply.github.com>
2022-05-15 16:09:27 -0400

    Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 
+function Input(props) {
+  return <input type={props.type} placeholder={props.placeholder} />;
+}
+
+function Login() {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+var isLoggedIn = true;
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
   );
 }
 
